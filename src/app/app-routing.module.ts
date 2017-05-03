9ce3609062e96df1d63d2364ecfb5711a4b092d1
Dominik Ostrowski <dominik.ostrowski@live.com>
2017-05-03 16:51:42 +0200

    Add loading ExampleModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { ExampleModule } from './example/example.module';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './example/example.module#ExampleModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

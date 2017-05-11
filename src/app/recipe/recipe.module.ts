0fa07efcbe490c183721b4c5c063d4fa6a5d5a9c
Adam Konopka <adam@MacBook-Air-Adam.local>
2017-05-11 16:48:06 +0200

    Create component to create recipes

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';;
 
 import { RecipeRoutingModule } from './recipe-routing.module';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RecipeRoutingModule
   ],
-  declarations: []
+  declarations: [CreateComponent]
 })
 export class RecipeModule { }

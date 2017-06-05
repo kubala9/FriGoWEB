7c3cdb3dc36b6dbc83a48eb0a28117f23be71e43
Dominik Ostrowski <dominik.ostrowski@live.com>
2017-06-05 19:48:07 +0200

    Add notifier in login component

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
 import { UserService } from '../../core/user.service';
 import { LoginModel } from '../../shared/models/account/login.model';
+import { NotifierService } from '../../core/notifier.service';
 
 @Component({
   selector: 'fg-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   loginModel: LoginModel = new LoginModel();
 
   constructor(
-    private accountService: AccountService
+    private accountService: AccountService,
+    private notifier: NotifierService
   ) { }
 
   ngOnInit() {
@@ -22,6 +24,10 @@ export class LoginComponent implements OnInit {
   login() {
     this.accountService
       .login(this.loginModel)
-      .subscribe(console.log, console.log); //TODO
+      .subscribe(() => {
+        this.notifier.success("Zalogowano!");
+      }, (error) => {
+        this.notifier.error(error);
+      });
   }
 }

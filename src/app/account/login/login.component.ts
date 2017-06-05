f8c126ae8a8e49727b7304146139d07d9d730348
Dominik Ostrowski <dominik.ostrowski@live.com>
2017-06-05 18:49:42 +0200

    Add account and register simple components

---
+++ src/app/account/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { UserService } from '../../core/user.service';
+import { LoginModel } from '../../shared/models/account/login.model';
+
+@Component({
+  selector: 'fg-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  loginModel: LoginModel = new LoginModel();
+
+  constructor(
+    private accountService: AccountService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.accountService
+      .login(this.loginModel)
+      .subscribe(console.log, console.log);
+  }
+}
---
+++ src/app/account/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { RegisterModel } from '../../shared/models/account/register.model';
+
+@Component({
+  selector: 'fg-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.sass']
+})
+export class RegisterComponent implements OnInit {
+  registerModel: RegisterModel = new RegisterModel();
+
+  constructor(
+    private accountService: AccountService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.accountService
+      .register(this.registerModel)
+      .subscribe(console.log, console.log);
+  }
+}

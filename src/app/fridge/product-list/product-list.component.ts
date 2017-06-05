105ba2ca26c62bb4b8c701d22f958f5067634b60
kubala9 <kuba100040@gmail.com>
2017-06-05 21:04:55 +0200

    lista produktow - akcje dominika

--- src/app/fridge/product-list/product-list.component.ts
+++ src/app/fridge/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FridgeService } from '../fridge.service';
+import { IngredientQuantity } from '../../shared/models/ingredient-quantity/ingredient-quantity';
 
 @Component({
   selector: 'fg-product-list',
@@ -6,7 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.sass']
 })
 
-
 export class ProductListComponent {
+  rows: IngredientQuantity[] = [];
+  selected = [];
 
+  constructor(
+    private fridge: FridgeService
+  ) { }
+
+
+  ngOnInit() {
+    this.fridge.fridgeContent$
+      .subscribe(content => {
+        this.rows = content
+      });
+    this.fridge.fetchFridgeContent();
+  }
+
+  onSelect({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+
+  delete() {
+    this.fridge.delete(this.selected.splice(0))
+      .subscribe(console.log, console.log); //TODO
+  }
 }
